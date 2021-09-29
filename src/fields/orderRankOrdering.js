72f47812097fe2b8975a0c306bb80ee959c1537a
Simeon Griggs <simeon@hey.com>
2021-09-29 07:00:36 +0100

    fix exported ordering direction

--- src/fields/orderRankOrdering.js
+++ src/fields/orderRankOrdering.js
@@ -3,5 +3,5 @@ import {ORDER_FIELD_NAME} from '../helpers/constants'
 export const orderRankOrdering = {
   title: 'Ordered',
   name: 'ordered',
-  by: [{field: ORDER_FIELD_NAME, direction: 'desc'}],
+  by: [{field: ORDER_FIELD_NAME, direction: 'asc'}],
 }
