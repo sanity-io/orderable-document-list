ac91e2d957b817e4c9f4e81858f4e7db600d85f7
andrewmumblebee <aceharris@hotmail.co.uk>
2024-04-16 14:40:58 +0100

    feat: export OrderableDocumentList component

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import {
   orderableDocumentListDeskItem,
   type OrderableListConfig,
 } from './desk-structure/orderableDocumentListDeskItem'
+import OrderableDocumentList from './OrderableDocumentList'
 
 export type {OrderableListConfig, RankFieldConfig}
-export {orderRankField, orderRankOrdering, orderableDocumentListDeskItem}
+export {orderRankField, orderRankOrdering, orderableDocumentListDeskItem, OrderableDocumentList}
